ca658291b1ee2f93b5a1c60f081eda34916efc50
Neel Jain <ndmnr4219@gmail.com>
2025-05-13 16:39:45 -0400

    update print statement for chat template

--- verl/trainer/main_ppo.py
+++ verl/trainer/main_ppo.py
@@ -207,7 +207,7 @@ def create_rl_dataset(data_paths, data_config, tokenizer, processor):
     else:
         dataset_cls = RLHFDataset
     print(f"Using dataset class: {dataset_cls.__name__}")
-    print(f"chat_template_func: {tokenizer.chat_template}")
+    print(f"{tokenizer}")
     dataset = dataset_cls(
         data_files=data_paths,
         tokenizer=tokenizer,
