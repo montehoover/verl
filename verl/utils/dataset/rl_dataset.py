c16c4a3d799b547f1ae979553537c00bad31c3cb
Neel Jain <ndmnr4219@gmail.com>
2025-05-13 23:24:04 -0400

    remove raw outputs print

--- verl/utils/dataset/rl_dataset.py
+++ verl/utils/dataset/rl_dataset.py
@@ -196,7 +196,6 @@ class RLHFDataset(Dataset):
 
         else:
             raw_prompt = self.tokenizer.apply_chat_template(messages, add_generation_prompt=True, tokenize=False)
-            print(f"raw_prompt: {raw_prompt}")
             model_inputs = self.tokenizer(raw_prompt, return_tensors="pt", add_special_tokens=False)
             input_ids = model_inputs.pop("input_ids")
             attention_mask = model_inputs.pop("attention_mask")
