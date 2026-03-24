25dcb79061610adf723c037362fb371e7624fdb8
Andrew Zheng <jandrew.zheng@gmail.com>
2026-03-24 16:49:52 -0400

    Adding updated launch-A6000 script

--- launch-A6000.sh
+++ launch-A6000.sh
@@ -1,13 +1,106 @@
 # launch launch.sh --classical_logfile_names --gpu_type rtxa6000 --mem 123 --timelimit 48 --gpus 2
 
 python run_grpo.py \
-    --model DynaGuard/DynaGuard-8B-6750 \
+    --model Qwen/Qwen3-8B \
     --dataset ahans1/code_patrol_postprocessed \
     --data_download_dir data/code_patrol \
     --subset v2 --lr 1e-6 --batch_size 4 --rollout_batch_size 16 --vllm_model_shards 2 \
-    --vllm_cache_utilization 0.5 \
+    --vllm_cache_utilization 0.6 \
     --num_generations 3 --max_prompt_length 8192 --max_response_length 1024 \
     --no-offload_weights_and_states \
     --wandb_entity azheng15-umd \
-    --wandb_project verl
+    --wandb_project DynaGuard2 \
+    --checkpoint_dir /fs/cml-projects/guardian_models/verl/Andrew_runs3 \
+    --resume_training
+
+python run_grpo.py \
+    --model Qwen/Qwen3-8B \
+    --dataset ahans1/code_patrol_postprocessed \
+    --data_download_dir data/code_patrol \
+    --subset v3 --lr 1e-6 --batch_size 4 --rollout_batch_size 16 --vllm_model_shards 2 \
+    --vllm_cache_utilization 0.6 \
+    --num_generations 3 --max_prompt_length 8192 --max_response_length 1024 \
+    --no-offload_weights_and_states \
+    --wandb_entity azheng15-umd \
+    --wandb_project DynaGuard2 \
+    --checkpoint_dir /fs/cml-projects/guardian_models/verl/Andrew_runs3 \
+    --resume_training
+
+python run_grpo.py \
+    --model Qwen/Qwen3-8B \
+    --dataset ahans1/code_patrol_postprocessed \
+    --data_download_dir data/code_patrol \
+    --subset v4 --lr 1e-6 --batch_size 4 --rollout_batch_size 16 --vllm_model_shards 2 \
+    --vllm_cache_utilization 0.6 \
+    --num_generations 3 --max_prompt_length 8192 --max_response_length 1024 \
+    --no-offload_weights_and_states \
+    --wandb_entity azheng15-umd \
+    --wandb_project DynaGuard2 \
+    --checkpoint_dir /fs/cml-projects/guardian_models/verl/Andrew_runs3 \
+    --resume_training
+
+python run_grpo.py \
+    --model Qwen/Qwen3-8B \
+    --dataset ahans1/code_patrol_postprocessed \
+    --data_download_dir data/code_patrol \
+    --subset v5 --lr 1e-6 --batch_size 4 --rollout_batch_size 16 --vllm_model_shards 2 \
+    --vllm_cache_utilization 0.6 \
+    --num_generations 3 --max_prompt_length 8192 --max_response_length 1024 \
+    --no-offload_weights_and_states \
+    --wandb_entity azheng15-umd \
+    --wandb_project DynaGuard2 \
+    --checkpoint_dir /fs/cml-projects/guardian_models/verl/Andrew_runs3 \
+    --resume_training
+
+python run_grpo.py \
+    --model Qwen/Qwen3-8B \
+    --dataset ahans1/code_patrol_postprocessed \
+    --data_download_dir data/code_patrol \
+    --subset v6 --lr 1e-6 --batch_size 4 --rollout_batch_size 16 --vllm_model_shards 2 \
+    --vllm_cache_utilization 0.6 \
+    --num_generations 3 --max_prompt_length 8192 --max_response_length 1024 \
+    --no-offload_weights_and_states \
+    --wandb_entity azheng15-umd \
+    --wandb_project DynaGuard2 \
+    --checkpoint_dir /fs/cml-projects/guardian_models/verl/Andrew_runs3 \
+    --resume_training
+
+python run_grpo.py \
+    --model Qwen/Qwen3-8B \
+    --dataset ahans1/code_patrol_postprocessed \
+    --data_download_dir data/code_patrol \
+    --subset v7 --lr 1e-6 --batch_size 4 --rollout_batch_size 16 --vllm_model_shards 2 \
+    --vllm_cache_utilization 0.6 \
+    --num_generations 3 --max_prompt_length 8192 --max_response_length 1024 \
+    --no-offload_weights_and_states \
+    --wandb_entity azheng15-umd \
+    --wandb_project DynaGuard2 \
+    --checkpoint_dir /fs/cml-projects/guardian_models/verl/Andrew_runs3 \
+    --resume_training
+
+python run_grpo.py \
+    --model Qwen/Qwen3-8B \
+    --dataset ahans1/code_patrol_postprocessed \
+    --data_download_dir data/code_patrol \
+    --subset v8 --lr 1e-6 --batch_size 4 --rollout_batch_size 16 --vllm_model_shards 2 \
+    --vllm_cache_utilization 0.6 \
+    --num_generations 3 --max_prompt_length 8192 --max_response_length 1024 \
+    --no-offload_weights_and_states \
+    --wandb_entity azheng15-umd \
+    --wandb_project DynaGuard2 \
+    --checkpoint_dir /fs/cml-projects/guardian_models/verl/Andrew_runs3 \
+    --resume_training
+
+python run_grpo.py \
+    --model Qwen/Qwen3-8B \
+    --dataset ahans1/code_patrol_postprocessed \
+    --data_download_dir data/code_patrol \
+    --subset v9 --lr 1e-6 --batch_size 4 --rollout_batch_size 16 --vllm_model_shards 2 \
+    --vllm_cache_utilization 0.6 \
+    --num_generations 3 --max_prompt_length 8192 --max_response_length 1024 \
+    --no-offload_weights_and_states \
+    --wandb_entity azheng15-umd \
+    --wandb_project DynaGuard2 \
+    --checkpoint_dir /fs/cml-projects/guardian_models/verl/Andrew_runs3 \
+    --resume_training
 
